0210925609338ad81a5e3bdf222acf4fb2da9d30
Rodion <roksis72010@gmail.com>
2024-10-24 15:56:00 +0200

    Software.h

---
+++ Software.cpp
@@ -0,0 +1,31 @@
+#include "Software.h"
+
+Software::Software()
+{
+}
+
+Software::Software(string name, string company)
+{
+}
+
+Software::~Software()
+{
+}
+
+string Software::getName() const
+{
+    return string();
+}
+
+string Software::getCompany() const
+{
+    return string();
+}
+
+void Software::setName(string name)
+{
+}
+
+void Software::setCompany(string company)
+{
+}
---
+++ Software.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+class Software
+{
+protected:
+	string name;
+	string company;
+
+public:
+	Software();
+	Software(string name,
+		string company);
+
+	virtual ~Software();
+
+	string getName()const;
+	string getCompany()const;
+
+	void setName(string name);
+	void setCompany(string company);
+
+	virtual void showInfo()const = 0;
+	virtual bool isAvalible()const = 0;
+};
+
