6029975d73ba74dbf8eee6280c02bc10ecba63f9
Rodion <roksis72010@gmail.com>
2024-10-24 16:49:23 +0200

    Main (UPDATE)

--- Lesson8.cpp
+++ Lesson8.cpp
@@ -1,10 +1,13 @@
 ﻿
 #include "Free.h"
 #include "Software.h"
+#include "Shareware.h"
 
 int main()
 {
     Free soft("Spotify","Spotify Technology S.A.");
+    Shareware soft2("Photoshop", "ADOBE", Date()-10, 7);
 
     soft.showInfo();
+    soft2.showInfo();
 }
